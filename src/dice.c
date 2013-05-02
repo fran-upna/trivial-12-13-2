52e0731f1520b83ce85e15c4edf773114188f5cc
AlvaroLarumbe <biobiotm@gmail.com>
2013-05-02 15:26:42 +0200

    Libreria para tirar dado y recibir dos categorías

---
+++ src/dice.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+void envio_cat(char *category1[], char *category2[], int result);
+int tirarDado(FILE *sock, char *cat1[], char *cat2[]);
+void categoria(int x, char *cat[]);
+
+int main (int argc, char* argv[]) {
+	int x;
+	// Establezco la semilla para aleatoriedad
+	srand(time(NULL));
+	x = rand() % 6 +1;
+	printf("El dado ha salido %d\n",x);
+	
+	char *cat1[20], *cat2[20];
+
+	tirarDado((FILE*)1,cat1,cat2);
+	
+	envio_cat(cat1,cat2,x);
+	
+}
+
+
+// Función para enviar las dos categorías y el resultado del dado al cliente
+// Entrada: un string con dos categorías y el resultado del dado
+// Salida: envía por el socket las categorías y el resultado del dado
+void envio_cat(char *category1[], char *category2[], int result){
+	char buf_info[50];
+	sprintf(buf_info,"%s %s %d\n", *category1, *category2, result);
+	//fprintf(fsock,"%s",buf_info);
+	printf("Se han enviado al jugador las categorías %s y %s.\n",*category1, *category2);
+	//bzero(buf_info);
+	
+	// Uso el socket para enviar toda la información
+}
+
+void categoria(int x, char *cat[]) {
+	switch(x) {
+		case 1: strcpy(*cat, "GEOGRAPHY");
+				break;
+		case 2: strcpy(*cat, "ENTERTAINMENT");
+				break;
+		case 3: strcpy(*cat, "HISTORY");
+				break;
+		case 4: strcpy(*cat, "LITERATURE");
+				break;
+		case 5: strcpy(*cat, "SCIENCE_NATURE");
+				break;
+		case 6: strcpy(*cat, "SPORTS");
+				break;
+		default: printf("La tirada no es válida\n");
+	}
+}
+
+int tirarDado(FILE *sock, char *cat1[], char *cat2[]) {
+	int x, y;
+	
+	// Calculamos categorías aleatorias
+	srand(time(NULL));
+	x = rand() % 6 + 1;
+	y = rand() % 6 + 1;
+	
+	// Nos aseguramos de que las categorías son distintas
+	while(x == y) {
+		y = rand();
+	}
+	
+	// Envíamos categoría
+	categoria(x, cat1);
+	categoria(x, cat2);
+}
