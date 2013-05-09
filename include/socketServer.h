f6f6f652cb97090389635619ebbc96a13ecaa054
Mikel Abaurrea Mateo <Kelmi_12@hotmail.es>
2013-05-09 12:53:12 +0200

    Creamos la librería que se encarga de crear el socket para el servidor

---
+++ include/socketServer.h
@@ -0,0 +1,10 @@
+//////////////////////////////////////////
+//Prototipos de funciones//
+/////////////////////////////////////////
+
+////Preparar conexión servidor.
+int prepararServidor(char p[],char m[]);
+////Aceptar conexion en servidor
+int esperaConexiones(int sock);
+
+
---
+++ src/socketServer.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+
+
+
+///////////////////////////////
+//Implementacion de funciones//
+///////////////////////////////
+
+////Preparar conexión servidor.
+int prepararServidor(char p[],char m[]){
+    
+    int sock,puerto,max;
+    struct sockaddr_in servidor;
+  
+    puerto=atoi(p);
+	max=atoi(m);
+    //Se crea el socket
+    sock=socket(PF_INET,SOCK_STREAM,0);
+    //DATOS DE SERVIDOR A CONECTARSE EL SOCKET//
+    //////////////////////////////////////////////////
+    servidor.sin_family=AF_INET;
+    servidor.sin_port=htons(puerto);
+    servidor.sin_addr.s_addr= htonl(INADDR_ANY);
+    /////////////////////////////////////////////////
+    
+    //AVISANDO AL SO DE QUE CREAMOS UN SERVIDOR
+    if (bind (sock, (struct sockaddr *)&servidor, sizeof (servidor)) == -1) { 
+	    printf ("Error:El puerto esta ocupado\n\n"); 
+	    exit(-2);
+    }
+    //AVISANDO AL SO DE QUE EMPIEZE A ESCUCHAR
+    printf("Escuchando conexiones en el puerto: %d\n",puerto);
+    if (listen (sock, max) == -1) { 
+	    printf ("Error creando servidor\n"); 
+	    exit(-2);
+    }
+
+    return sock;
+}
+
+//Aceptar conexion en servidor
+int esperaConexiones(int sock){
+     
+    struct sockaddr_in cliente;
+    unsigned int dirlen,cone=0;
+	  
+    dirlen=sizeof(cliente);
+    cone = accept (sock, (struct sockaddr *)&cliente,&dirlen); 
+    
+    if (sock == -1){ 
+	printf ("Erro aceptando conexionr\n"); 
+	exit(-2);
+    }
+    printf("Conexion establecida desde la IP %x con el puerto %d.\n----------------------------------------------\n",ntohl(cliente.sin_addr.s_addr),ntohs(cliente.sin_port));
+  
+    return cone;
+}
+
