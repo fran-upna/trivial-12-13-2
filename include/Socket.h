2946c6f22ffb1986566a39c2e2c22044111b1b80
Ruben <rubens_cpu@hotmail.com>
2013-05-16 13:32:02 +0200

    Añadimos la funcionalidad de preparar al cliente para establecer la conexion

--- include/Socket.h
+++ include/Socket.h
@@ -4,6 +4,8 @@
 
 ////Preparar conexión servidor.
 int Socket_prepararServidor(char p[],char m[]);
+////Preparar conexion cliente y establecer con serivor.
+int Socket_prepararCliente(char port[],char ip[])
 
 
 
