52a4ec5593fd312fa2c0ac03b3d29b9195117ca0
Ruben <rubens_cpu@hotmail.com>
2013-05-23 12:44:33 +0200

    Modificacion del trivial_cliente.c para pasar la ip y el puerto del servidor directamente desde los argumentos que se le pasan al programa

--- src/trivial_client.c
+++ src/trivial_client.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include "Socket.h"
 
 int main(int argc, char *argv[]) {
+  int sock;
+  if(argc !=2){
+    printf("Error. Uso: %s <ip_servidor> <puerto_servidor>\n");
+    exit(-1);
+  }
+  sock = Socket_prepararCliente(argv[1],argv[2]);
 
 	return 0;
 }
