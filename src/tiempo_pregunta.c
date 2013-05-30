5b13fc4bb9a71604ac8ef441ed19050ae297752c
KevinMarques <kevin.k93@hotmail.com>
2013-05-30 13:38:11 +0200

    Arreglado fallo de bloqueo en el conteo de los 10 segundos.

--- src/tiempo_pregunta.c
+++ src/tiempo_pregunta.c
@@ -1,40 +1,52 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#include <string.h>
 #include <unistd.h>
+#include <signal.h>
 #include <Socket.h>
+
+/* AVISO: La parte comentada corresponde a la parte de código que debe implementarse en el main del cliente*/
+
+/*pid_t pid_contador;
+
+int main(void){
+	
+	int res;
+	
+	printf("****Probaremos el conteo de 10 segundos****\n");
+	printf("Tienes 10 segundos para escribir un número y pulsar ENTER\n");
+	iniciarContador((FILE*)1);
+	// Aquí se leería la respuesta
+	if (kill(pid_contador, 0) == 0) {
+		kill(pid_contador,SIGINT);
+		printf("Bien hecho, te has salvado\n");
+	}.
+	
+	return 0;
+}
+*/
 
 // Función para contar los 10 segundos de una pregunta.
 // Entrada: socket
 // Salida: si pasan los 10 segundos, se manda "P_FAIL" al socket
-// 		   si no pasan los 10 segundos, no se envía nada
+// si no pasan los 10 segundos, no se envía nada
 void iniciarContador(FILE* sock) {
-	pid_t pid_contador = fork();
+	int seg=0;
+	pid_contador = fork();
 	if(pid_contador == 0) {
-		int seg = 0, ok = 0;
-		char buf[5];
-		bzero(buf, 5);
-		
-		while(seg < 10 && ok == 0) {
-			fgets(buf, 5, sock);
-			if(strncmp(buf, "P_OK",4) == 0) {
-				ok = 1;
-			}
-			sleep(1);
+		while (seg<10){
 			seg++;
+			sleep(1);
 		}
-
-		if(ok == 0) {
-			// Informamos de que han pasado los 10 segundos
-			printf("Pregunta fallida: han pasado los 10 segundos.");
-			socket_escribir(sock, "P_FAIL\n");
-		}
+		socket_escribir(sock, "P_FAIL\n");
+		printf("Se ha superado el límite de tiempo.\n");
+		kill(pid_contador,SIGINT);
 	}
+	
 }
-
+	
 // Parte correspondiente al servidor (para añadir a la función del grupo 1)
 /*char buf[] = socket_leer(sock);
 if (strncmp(buf,"P_FAIL\n",6)==0){
-	// Hacer lo mismo que se haría cuando la pregunta es incorrecta
+// Hacer lo mismo que se haría cuando la pregunta es incorrecta
 }*/
