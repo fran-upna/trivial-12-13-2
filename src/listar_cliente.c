6fdc745853378bd19bc4a35c6bec7aa5c24db6db
Ruben <rubens_cpu@hotmail.com>
2013-05-30 13:04:24 +0200

    Codigo adecuado a las funciones de leer y escribir de Socket.c .Falta comprobar que funciona adecuadamente. (deberia).

--- src/listar_cliente.c
+++ src/listar_cliente.c
@@ -6,24 +6,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-
-struct Usuario
-{
-    char *Alias;
-    char *id;
-    int resultado;
-};
-
-#define Fichero "Cliente.cfg"
-#define B 60
-#define M 60
-#define TM 50
-
-void escribir(int ,char *);
-void leer_dispo(int ,char *);
-void leer(int );
-void dividir(char **,char *,int );
-int cuentapalabras(char *);
+#include "Socket.h"
 
 
 /**PROGRAMA PRINCIPAL**/
@@ -102,44 +85,39 @@ int main(int argc,char *argv[])
 /**************************/
 /**FUNCIONES**/
 
-void escribir(int sock,char *mensaje)
-{
-  FILE *f;
-  f = fdopen(sock,"w+");
-  setbuf(f,NULL);
-  printf("Enviando %s",mensaje);
-  fputs(mensaje,f);
-//      fclose(f);
-}
-
-void leer_dispo(int sock,char *buffer)
-{
-  FILE *f;
-  f = fdopen(sock ,"r+" );
-  setbuf(f,NULL);
-  fgets(buffer,20,f);//Leer
-  printf("Mensaje del SERVER: %s",buffer);
-}
-void leer(int sock)
-{
-  FILE *f;
-  int tamano_buf = 60;
-  char *msg;
-  msg =(char *)malloc(40*sizeof(char));
-  f = fdopen( sock ,"w+" );
-  setbuf(f,NULL);
-  if(!f)
-  {
-   printf("Error FDOPEN\n"); 
-  }
-  fgets(msg,tamano_buf,f);//Leer
-  printf("Mensaje del SERVER <- <- %s",msg);
-//   fclose(f);
-}
 //s_disponibles X
 //x es el numero de partidas disponibles
 
-void dividir(char **resultado,char *cadena,int h)
+void listar_cliente_pdisponibles(int sock;)
+{
+  char *buffer, **resultado;
+  int size_buf = 30;
+  int npartidas,palabras;
+  char *nompartida;
+  buffer = (char *)malloc(size_buf*sizeof(char));
+  Socket_escribir(sock,"P_DISPONIBLES\n");
+  //escribir(sock,"P_DISPONIBLES\n");
+  buffer = Socket_leer(sock);
+  //leer_dispo(sock,buffer);
+  palabras = listar_cliente_cuentapalabras(buffer);
+  resultado = (char **)malloc(palabras*sizeof(char *));
+  for(i=0;i<palabras;i++) 
+    resultado[i]=(char *)malloc(size_buf*sizeof(char));
+  listar_cliente_dividir(resultado,buffer,palabras); 
+  npartidas = atoi(resultado[1]);
+  //printf("Numero de partidas disponibles = %d\n",npartidas);
+  nompartida = (char *)malloc(size_buf*sizeof(char));
+  printf("Estas son las %d partidas disponibles: \n",npartidas);
+  for(i=0;i<npartidas;i++)
+  {    
+     nompartida = Socket_leer(sock);
+     printf("%s",nompartida);
+  }  
+}
+
+
+
+void listar_cliente_dividir(char **resultado,char *cadena,int h)
 {
    int i=0,j=0,k=0;
   for(i=0;i<h;i++)
@@ -156,7 +134,7 @@ void dividir(char **resultado,char *cadena,int h)
   }
 }
 
-int cuentapalabras(char *cadena)
+int listar_cliente_cuentapalabras(char *cadena)
 {
  int cont,contp=0;
  for(cont=0;cont<(strlen(cadena));cont++) if(cadena[cont]==' ') contp++;
