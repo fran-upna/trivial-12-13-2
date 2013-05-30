8c47d7c75e5ca5dcfa1b0c5462f1f58802b5672f
KevinMarques <kevin.k93@hotmail.com>
2013-05-30 12:54:34 +0200

    Separada y mejorada la funcion de obtener categorías.

---
+++ include/categorias.h
@@ -0,0 +1,15 @@
+/* 
+Prototipo de la función categoria
+
+El primer parámetro indica el número de categoría a obtener, siendo:
+	1 - GEOGRAPHY
+	2 - ENTERTAINMENT
+	3 - HISTORY
+	4 - LITERATURE
+	5 - SCIENCE_NATURE
+	6- SPORTS
+	
+	El segundo parámetro es un array tipo char donde se almacenará la categoría obtenida.
+*/
+
+void categorias(int x, char cat[]);
---
+++ src/categorias.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <QuestionType.h>
+
+void categoria(int x, char cat[]) {
+	switch(x) {
+		case GEOGRAPHY: strcpy(cat, "GEOGRAPHY");
+				break;
+		case ENTERTAINMENT: strcpy(cat, "ENTERTAINMENT");
+				break;
+		case HISTORY: strcpy(cat, "HISTORY");
+				break;
+		case LITERATURE: strcpy(cat, "LITERATURE");
+				break;
+		case SCIENCE_NATURE: strcpy(cat, "SCIENCE_NATURE");
+				break;
+		case SPORTS: strcpy(cat, "SPORTS");
+				break;
+		default: printf("No existe categoría.\n");
+	}
+}
