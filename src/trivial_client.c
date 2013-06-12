b67c41ae12270213dcb2ed0c3ff0423dddf214a1
Asier Lucas <asierlucs@hotmail.com>
2013-06-12 17:02:07 +0200

    Correcciones para conseguir que el código compile tras el rebase de nuestra rama con master

--- src/trivial_client.c
+++ src/trivial_client.c
@@ -3,34 +3,36 @@
 #include <stdlib.h>
 
 #include <Registro.h>
+#include <Socket.h>
 
 int main(int argc, char *argv[]) {
-  char opcion;
-  char *datos;	
-  char l;
+  char opcion[2] = {0,0};
+  char buffer[50];
   //Crear el socket
-  printf("Bienvenido a nuestro TRIVIAL\n¿Que deseas hacer?\n 1.-Autentificarte\n 2.-Registrarte\n 3.-Salir\n);
-  scanf("%c",opcion);
+  printf("Bienvenido a nuestro TRIVIAL\n¿Que deseas hacer?\n 1.-Autentificarte\n 2.-Registrarte\n 3.-Salir\n");
+  scanf("%c", &opcion[0]);
   Socket_escribir(sock,opcion);
-  if (opcion=='1') {
+  if (strcmp(opcion, "1") == 0) {
   }
-  else if (opcion=='2'){ // Ha seleccionado la opcion de registrarse como nuevo usuario del trivial
+  else if (strcmp(opcion, "2") == 0){ // Ha seleccionado la opcion de registrarse como nuevo usuario del trivial
     char envio[50];
     Registro_altacliente(sock, envio); // Llama a la funcion que le pedirá su nombre y contraseña y le devuelve la informacion en datos
     
     Socket_escribir(sock,envio); // Envia por el socket al servidor su nombre y su clave, en una misma variable, datos.
     
-    l=Socket_leer(sock); // Recibe la respuesta del registro
+    buffer=Socket_leer(sock); // Recibe la respuesta del registro
     
-    if (l=='O'){ // Si recibe 'O' todo ha ido bien y si recibe 'E' ha fallado.
+    if (strcmp(buffer, "O") == 0){ // Si recibe 'O' todo ha ido bien y si recibe 'E' ha fallado.
       printf("Registro correcto\n");
     }
     else {
       printf("Registro incorrecto\n");
-      exit 0;
+      exit(0);
     }
   }
-  else if (opcion=='3'){
+  else if (strcmp(opcion, "3") == 0){
     return 0;
   }
+
+  return 0;
 }
