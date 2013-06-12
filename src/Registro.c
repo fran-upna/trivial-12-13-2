985c1fea562c48849f80a66e39c1d30ecb872afa
Asier Lucas <asierlucs@hotmail.com>
2013-06-12 16:12:47 +0200

    Creamos registro de usuario

--- src/Registro.c
+++ src/Registro.c
@@ -29,10 +29,22 @@ char Registro_altausuario(int socket, char *envio){
   // Recibimos el socket, y la identificacion del usuario y con estos datos
   // llamamos a la funcion que los guarda en el fichero de registro
   // Devuelve 1 si se ha dado de alta correctamente y 0 si no.
-  char *usuario, *clave;
-  usuario=strtok(envio," ");
-  // sacar la clave
-  clave=strtok(envio,0);
+  char usuario[40], clave[40];
+  int i=0, a=0;
+  while(envio[i]!=' ') { // Extreamos el nombre del usuario.
+    usuario[a]=envio[i];
+    i++;
+  }
+  i++;
+  
+  // 0 para terminar cadena¿?¿?¿?¿?¿?¿?¿
+  
+  a=0;
+  while (envio[i] != 0) { // Extreamos la clave del usuario.
+    clave[a]=envio[i];
+    i++;
+    a++;
+  }
   FILE *ficheroservidor;
   if (Registro_comprobarusuarios(usuario)==1) {
     // No existia el usuariosregistrados  
@@ -53,14 +65,13 @@ char Registro_altausuario(int socket, char *envio){
 
 
 // PARTE CLIENTE ///////////////////////////////////////////////////////////////////
-char* Registro_altacliente(int socket){
+void Registro_altacliente(int socket, char* envio){
+  // Recibe el socket y pedirá al usuario su nombre y su clave para devolverla
+  // en la variable envio.
   char usuario[20], clave[20];
-  char envio[40];
-  //char respuesta[40];
   printf("\nIntroduce tu nombre de usuario: ");
   scanf("%s",usuario);
   printf("\nIntroduce tu contraseña: ");
   scanf("%s",clave);
   sprintf(envio,"%s %s",usuario,clave);
-  return(envio);
 }
