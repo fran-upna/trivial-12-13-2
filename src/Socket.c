51b568d0123313f067f894b2555a26007534e503
iaguas <aguas.71332@e.unavarra.es>
2013-06-18 21:49:31 +0200

    Corregido problemas con espacios para hacer una lectura más cómoda

--- src/Socket.c
+++ src/Socket.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
 #define TAM_BUFF 300
 
