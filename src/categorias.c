f0106f99edb10c774faa901ce471cb13e361ac7f
iaguas <aguas.71332@e.unavarra.es>
2013-06-19 14:25:57 +0200

    Añadidas las funciones para pedir y devolver las categorías entre servidor y cliente.

--- src/categorias.c
+++ src/categorias.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <QuestionType.h>
 
 void categorias(int x, char cat[]) {
@@ -19,3 +20,33 @@ void categorias(int x, char cat[]) {
 		default: printf("No existe categoría.\n");
 	}
 }
+
+// Función devolverCategorias para colocar en el lado del Servidor y devolver las categorias de juego del trivial
+// Entrada: un entero que indica el socket al que se le deben devolver las categorías.
+// Requisitos: haber recibido el mensaje C CATEGORY LIST desde el cliente.
+// Salida: ninguna
+void devolverCategorias(int socket){
+	//if (strstr(entrada,"C CATEGORY LIST")!=NULL) Se tiene que comprobar fuera y llamar a la función si es necesario.
+		Socket_escribir(socket,"LIST GEOGRAPHY ENTERTAINMENT HISTORY LITERATURE SCIENCE_NATURE SPORTS");
+}
+
+// Función pedirCategorias para colocar en el lado del Cliente y que muestra por pantalla las categorías devueltas por el servidor.
+// Entrada: un entero que indica el socket del servidor con el que se va a comunicar.
+// Requisitos: no encontrarse en medio de ninguna otra comunicación con el servidor.
+// Salida: ninguna.
+void pedirCategorias(int socket){
+	char* buff[300], cad;
+
+	Socket_escribir(socket, "C CATEGORY LIST");
+	buff = Socket_leer(socket);
+
+	puts("Las categorías de las que dispone el juego son las siguientes:");
+	cad = strtok(buff, " \n");    // Primer token -> LIST
+	if((cad = strtok( NULL, " \n"))==NULL) // Primera llamada para comprobar si no hay categorias.
+		puts("No hay mas usuarios. Solamente usted se encuentra conectado");
+	else 
+		printf("\t%s\n", cad);
+   	while((cad = strtok( NULL, " \n")) != NULL)  // Posteriores llamadas
+     		printf( "\t%s\n", cad );
+}
+	
