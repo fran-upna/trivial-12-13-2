1d50bd8dbccf131552beef3cacddd79606b372eb
iaguas <aguas.71332@e.unavarra.es>
2013-06-19 17:58:12 +0200

    Solucionados fallos menores en el Socket_leer y en las librerias que utiliza el fichero

--- src/Socket.c
+++ src/Socket.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -124,6 +126,7 @@ int Socket_escribir(FILE* sock, char buf[]) {
 
 char* Socket_leer(int sock){
     char buf[TAM_BUFF];
+    FILE* f;
 
     f=fdopen(sock,"r");
     setbuf(f,NULL);
